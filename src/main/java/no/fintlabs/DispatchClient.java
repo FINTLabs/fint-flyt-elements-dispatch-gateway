ddfc1e8c83561e92c6d1efe936430a7de6803a63
Eivind Morch <eivind@morch.priv.no>
2022-10-25 18:01:45 +0200

    Add temporary kafka publishing for debug

--- src/main/java/no/fintlabs/DispatchClient.java
+++ src/main/java/no/fintlabs/DispatchClient.java
@@ -3,14 +3,22 @@ package no.fintlabs;
 import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.arkiv.noark.JournalpostResource;
 import no.fint.model.resource.arkiv.noark.SakResource;
+import no.fintlabs.kafka.TempKafkaDispatchProducerService;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class DispatchClient {
 
+    private final TempKafkaDispatchProducerService tempKafkaDispatchProducerService;
+
+    public DispatchClient(TempKafkaDispatchProducerService tempKafkaDispatchProducerService) {
+        this.tempKafkaDispatchProducerService = tempKafkaDispatchProducerService;
+    }
+
     public Object dispatchNewCase(SakResource sakResource) {
         log.info("Dispatching sakResource=" + sakResource.toString());
+        tempKafkaDispatchProducerService.publish(sakResource);
         return new Object();
     }
 
---
+++ src/main/java/no/fintlabs/kafka/TempKafkaDispatchProducerService.java
@@ -0,0 +1,41 @@
+package no.fintlabs.kafka;
+
+import no.fint.model.resource.arkiv.noark.SakResource;
+import no.fintlabs.kafka.event.EventProducer;
+import no.fintlabs.kafka.event.EventProducerFactory;
+import no.fintlabs.kafka.event.EventProducerRecord;
+import no.fintlabs.kafka.event.topic.EventTopicNameParameters;
+import no.fintlabs.kafka.event.topic.EventTopicService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TempKafkaDispatchProducerService {
+
+    EventProducer<SakResource> newSakResourceEventProducer;
+    EventTopicNameParameters topicNameParameters;
+
+    public TempKafkaDispatchProducerService(
+            EventTopicService eventTopicService,
+            EventProducerFactory eventProducerFactory
+    ) {
+        topicNameParameters = EventTopicNameParameters
+                .builder()
+                .eventName("temp-dispatch-new-case-to-elements")
+                .build();
+
+        eventTopicService.ensureTopic(topicNameParameters, 0);
+
+        newSakResourceEventProducer = eventProducerFactory
+                .createProducer(SakResource.class);
+    }
+
+    public void publish(SakResource sakResource) {
+        newSakResourceEventProducer.send(
+                EventProducerRecord
+                        .<SakResource>builder()
+                        .topicNameParameters(topicNameParameters)
+                        .value(sakResource)
+                        .build()
+        );
+    }
+}
