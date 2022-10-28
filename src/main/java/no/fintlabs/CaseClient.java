dd471e8bd4de0c9dbe285706411a45b837766d41
Eivind Morch <eivind@morch.priv.no>
2022-10-28 14:40:36 +0200

    Add collection case dispatching

---
+++ src/main/java/no/fintlabs/CaseClient.java
@@ -0,0 +1,109 @@
+package no.fintlabs;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fint.model.resource.arkiv.noark.SakResource;
+import no.fintlabs.model.Result;
+import no.fintlabs.model.Status;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.Duration;
+
+@Slf4j
+@Service
+public class CaseClient {
+
+    private final WebClient webClient;
+
+    public CaseClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<SakResource> getCase(String archiveCaseId) {
+        return webClient
+                .get()
+                .uri("/arkiv/noark/sak/" + archiveCaseId)
+                .retrieve()
+                .bodyToMono(SakResource.class);
+    }
+
+    public Mono<Result> postCase(SakResource sakResource) {
+        return pollForResult(
+                webClient
+                        .post()
+                        .uri("/arkiv/noark/sak")
+                        .bodyValue(sakResource)
+                        .retrieve()
+        );
+    }
+
+    public Mono<Result> putCase(String archiveCaseId, SakResource sakResource) {
+        return pollForResult(
+                webClient
+                        .put()
+                        .uri("/arkiv/noark/sak/" + archiveCaseId)
+                        .bodyValue(sakResource)
+                        .retrieve()
+        );
+    }
+
+    private Mono<Result> pollForResult(WebClient.ResponseSpec responseSpec) {
+        return responseSpec
+                .toBodilessEntity()
+                .<URI>handle((entity, sink) -> {
+                            if (HttpStatus.ACCEPTED.equals(entity.getStatusCode())
+                                    && entity.getHeaders().getLocation() != null) {
+                                sink.next(entity.getHeaders().getLocation());
+                            } else {
+                                sink.error(new RuntimeException("Expected 202 Accepted response with redirect header"));
+                            }
+                        }
+                )
+                .delayElement(Duration.ofMillis(200))
+                .flatMap(this::pollForCreatedLocation)
+                .flatMap(this::getCase)
+                .map(resultSakResource -> new Result(Status.ACCEPTED, resultSakResource.getMappeId().getIdentifikatorverdi()))
+                .doOnError(e -> {
+                    if (e instanceof WebClientResponseException) {
+                        log.error(e + " body=" + ((WebClientResponseException) e).getResponseBodyAsString());
+                    } else {
+                        log.error(e.toString());
+                    }
+                })
+                .onErrorResume(WebClientResponseException.class, e ->
+                        Mono.just(new Result(
+                                e.getStatusCode().is4xxClientError()
+                                        ? Status.DECLINED
+                                        : Status.FAILED,
+                                null
+                        ))
+                )
+                .onErrorReturn(RuntimeException.class, new Result(Status.FAILED, null));
+    }
+
+    private Mono<URI> pollForCreatedLocation(URI uri) {
+        return webClient
+                .get()
+                .uri(uri)
+                .retrieve()
+                .toBodilessEntity()
+                .filter(entity -> HttpStatus.CREATED.equals(entity.getStatusCode()) && entity.getHeaders().getLocation() != null)
+                .mapNotNull(entity -> entity.getHeaders().getLocation())
+                .repeatWhenEmpty(10, longFlux -> Flux.interval(Duration.ofSeconds(1)))
+                .switchIfEmpty(Mono.error(new RuntimeException("Reached max number of retries for polling for 201 Created location header")));
+    }
+
+    private Mono<SakResource> getCase(URI uri) {
+        return webClient
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(SakResource.class);
+    }
+
+}
--- src/main/java/no/fintlabs/DispatchClient.java
+++ src/main/java/no/fintlabs/DispatchClient.java
@@ -6,97 +6,35 @@ import no.fint.model.resource.arkiv.noark.SakResource;
 import no.fintlabs.kafka.TempKafkaDispatchProducerService;
 import no.fintlabs.model.Result;
 import no.fintlabs.model.Status;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
-import java.time.Duration;
 
 @Slf4j
 @Service
 public class DispatchClient {
 
-
-    private final WebClient webClient;
+    private final CaseClient caseClient;
     // TODO: 27/10/2022 Remove
     private final TempKafkaDispatchProducerService tempKafkaDispatchProducerService;
 
-    public DispatchClient(WebClient webClient, TempKafkaDispatchProducerService tempKafkaDispatchProducerService) {
-        this.webClient = webClient;
+    public DispatchClient(CaseClient caseClient, TempKafkaDispatchProducerService tempKafkaDispatchProducerService) {
+        this.caseClient = caseClient;
         this.tempKafkaDispatchProducerService = tempKafkaDispatchProducerService;
     }
 
     public Mono<Result> dispatchNewCase(SakResource sakResource) {
         tempKafkaDispatchProducerService.publishNewCaseData(sakResource);
-        return postCase(sakResource)
-                .delayElement(Duration.ofMillis(200))
-                .flatMap(this::getCreatedRedirect)
-                .flatMap(this::getCreatedCase)
-                .map(resultSakResource -> new Result(Status.ACCEPTED, resultSakResource.getMappeId().getIdentifikatorverdi()))
-                .doOnError(e -> {
-                    if (e instanceof WebClientResponseException) {
-                        log.error(e + " body=" + ((WebClientResponseException) e).getResponseBodyAsString());
-                    } else {
-                        log.error(e.toString());
-                    }
-                })
-                .onErrorResume(WebClientResponseException.class, e ->
-                        Mono.just(new Result(
-                                e.getStatusCode().is4xxClientError()
-                                        ? Status.DECLINED
-                                        : Status.FAILED,
-                                null
-                        ))
-                )
-                .onErrorReturn(RuntimeException.class, new Result(Status.FAILED, null));
-    }
-
-    private Mono<URI> postCase(SakResource sakResource) {
-        return webClient
-                .post()
-                .uri("/arkiv/noark/sak")
-                .bodyValue(sakResource)
-                .retrieve()
-                .toBodilessEntity()
-                .handle((entity, sink) -> {
-                            if (HttpStatus.ACCEPTED.equals(entity.getStatusCode())
-                                    && entity.getHeaders().getLocation() != null) {
-                                sink.next(entity.getHeaders().getLocation());
-                            } else {
-                                sink.error(new RuntimeException("Expected 202 Accepted response with redirect header"));
-                            }
-                        }
-                );
-    }
-
-    private Mono<URI> getCreatedRedirect(URI uri) {
-        return webClient
-                .get()
-                .uri(uri)
-                .retrieve()
-                .toBodilessEntity()
-                .filter(entity -> HttpStatus.CREATED.equals(entity.getStatusCode()) && entity.getHeaders().getLocation() != null)
-                .mapNotNull(entity -> entity.getHeaders().getLocation())
-                .repeatWhenEmpty(10, longFlux -> Flux.interval(Duration.ofSeconds(1)))
-                .switchIfEmpty(Mono.error(new RuntimeException("Reached max number of retries for getting a 201 Created location header")));
-    }
-
-    private Mono<SakResource> getCreatedCase(URI uri) {
-        return webClient
-                .get()
-                .uri(uri)
-                .retrieve()
-                .bodyToMono(SakResource.class);
+        return caseClient.postCase(sakResource);
     }
 
     public Mono<Result> dispatchToCollectionCase(String collectionCaseId, JournalpostResource journalpostResource) {
         tempKafkaDispatchProducerService.publishCollectionCaseData(collectionCaseId, journalpostResource);
-        log.info("Dispatching to collection case: collectionCaseId= '" + collectionCaseId + "' journalpostResource=" + journalpostResource.toString());
-        return Mono.just(new Result(Status.FAILED, null));
+        return caseClient.getCase(collectionCaseId)
+                .map(sakResource -> {
+                    sakResource.getJournalpost().add(journalpostResource);
+                    return sakResource;
+                })
+                .flatMap(sakResource -> caseClient.putCase(collectionCaseId, sakResource));
     }
 
     public Mono<Result> dispatchToExistingOrAsNewCase(SakResource sakResource) {
