19a8aa09d248acb8ab2784594c43e7d7159cdc34
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-07-25 12:26:39 +0200

    bugfix make adresselinje able to take both String and Collection<String>

--- src/main/java/no/fintlabs/model/instance/AdresseDto.java
+++ src/main/java/no/fintlabs/model/instance/AdresseDto.java
@@ -1,10 +1,18 @@
 package no.fintlabs.model.instance;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Builder;
 import lombok.extern.jackson.Jacksonized;
 
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Builder
@@ -14,6 +22,7 @@ public class AdresseDto {
     private final String postnummer;
     private final String poststed;
 
+    @JsonDeserialize(using = AdresselinjeDeserializer.class)
     public Optional<Collection<String>> getAdresselinje() {
         return Optional.ofNullable(adresselinje);
     }
@@ -24,6 +33,24 @@ public class AdresseDto {
 
     public Optional<String> getPoststed() {
         return Optional.ofNullable(poststed);
+    }
+
+    public static class AdresselinjeDeserializer extends JsonDeserializer<Collection<String>> {
+
+        @Override
+        public Collection<String> deserialize(JsonParser jp, DeserializationContext ctxt)
+                throws IOException {
+            JsonNode node = jp.getCodec().readTree(jp);
+            List<String> addresses = new ArrayList<>();
+            if (node.isArray()) {
+                for (JsonNode n : node) {
+                    addresses.add(n.asText());
+                }
+            } else if (node.isTextual()) {
+                addresses.add(node.asText());
+            }
+            return addresses;
+        }
     }
 
 }
