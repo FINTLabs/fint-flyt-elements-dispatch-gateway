0e3607e47844f12650882fd3fbff9ae7dcfd539b
Eivind Morch <eivind@morch.priv.no>
2023-09-20 15:49:29 +0200

    FFS-285 #comment Change dispatch request to event topic

---
+++ src/main/java/no/fintlabs/kafka/error/InstanceDispatchingErrorHandlerService.java
@@ -0,0 +1,35 @@
+package no.fintlabs.kafka.error;
+
+import no.fintlabs.exceptions.InstanceDispatchDeclinedException;
+import no.fintlabs.flyt.kafka.InstanceFlowErrorHandler;
+import no.fintlabs.flyt.kafka.headers.InstanceFlowHeaders;
+import no.fintlabs.flyt.kafka.headers.InstanceFlowHeadersMapper;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InstanceDispatchingErrorHandlerService extends InstanceFlowErrorHandler {
+
+    private final InstanceDispatchingErrorProducerService instanceDispatchingErrorProducerService;
+
+    protected InstanceDispatchingErrorHandlerService(
+            InstanceFlowHeadersMapper instanceFlowHeadersMapper,
+            InstanceDispatchingErrorProducerService instanceDispatchingErrorProducerService
+    ) {
+        super(instanceFlowHeadersMapper);
+        this.instanceDispatchingErrorProducerService = instanceDispatchingErrorProducerService;
+    }
+
+    @Override
+    public void handleInstanceFlowRecord(Throwable cause, InstanceFlowHeaders instanceFlowHeaders, ConsumerRecord<?, ?> consumerRecord) {
+        if (cause instanceof InstanceDispatchDeclinedException) {
+            instanceDispatchingErrorProducerService.publishInstanceDispatchDeclinedErrorEvent(
+                    instanceFlowHeaders,
+                    (InstanceDispatchDeclinedException) cause
+            );
+        } else {
+            instanceDispatchingErrorProducerService.publishGeneralSystemErrorEvent(instanceFlowHeaders);
+        }
+    }
+
+}
