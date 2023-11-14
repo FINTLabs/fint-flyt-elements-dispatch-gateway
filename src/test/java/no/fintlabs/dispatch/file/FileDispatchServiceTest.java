ec3dfab62ce0ac489cffd26bf8d0d42934630763
Egil Ballestad <egil.ballestad@vigoiks.no>
2023-11-14 14:06:34 +0100

    wip unittests for internally generated error messages

--- src/test/java/no/fintlabs/dispatch/file/FileDispatchServiceTest.java
+++ src/test/java/no/fintlabs/dispatch/file/FileDispatchServiceTest.java
@@ -1,5 +1,7 @@
 package no.fintlabs.dispatch.file;
 
+import lombok.AllArgsConstructor;
+import no.fint.model.resource.Link;
 import no.fintlabs.dispatch.file.result.FilesDispatchResult;
 import no.fintlabs.model.File;
 import no.fintlabs.model.instance.DokumentbeskrivelseDto;
@@ -17,9 +19,10 @@ import reactor.test.StepVerifier;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.IntStream;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static java.util.stream.Collectors.toMap;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -43,30 +46,192 @@ class FileDispatchServiceTest {
 
     @Test
     public void givenSingleDokumentBeskrivelseWithSingleDokumentObjektShouldReturnAcceptedResultWithSingleDispatch() {
+        FileMock fileMock = mockFile();
 
-        UUID fileId = mock(UUID.class);
-        File file = mock(File.class);
-
-        when(fileClient.getFile(fileId)
-                .thenReturn(
-                        Mono.just(file)
-                ));
-
-        StepVerifier.create(
-                fileDispatchService.dispatchFiles(List.of(
-                        DokumentbeskrivelseDto
+        DokumentbeskrivelseDto dokumentbeskrivelseDto = DokumentbeskrivelseDto
+                .builder()
+                .dokumentobjekt(List.of(
+                        DokumentobjektDto
                                 .builder()
-                                .dokumentobjekt(List.of(
-                                        DokumentobjektDto
-                                                .builder()
-                                                .fileId(fileId)
-                                                .build()
-                                ))
+                                .fileId(fileMock.fileId)
                                 .build()
                 ))
-        ).expectNext(FilesDispatchResult.accepted(
+                .build();
 
-        ))
+        StepVerifier.create(
+                        fileDispatchService.dispatchFiles(List.of(dokumentbeskrivelseDto))
+                )
+                .expectNext(FilesDispatchResult.accepted(Map.of(fileMock.fileId, fileMock.archiveLink)))
+                .verifyComplete();
+
+        verify(fileClient, times(1)).getFile(fileMock.fileId);
+        verify(fintArchiveClient, times(1)).postFile(fileMock.file);
+    }
+
+    @Test
+    public void givenSingleDokumentBeskrivelseWithMultipleDokumentObjektShouldReturnAcceptedResultWithMultipleDispatch() {
+        List<FileMock> fileMocks = mockFiles(2);
+
+        DokumentbeskrivelseDto dokumentbeskrivelseDto = DokumentbeskrivelseDto
+                .builder()
+                .dokumentobjekt(List.of(
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(0).fileId)
+                                .build(),
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(1).fileId)
+                                .build()
+                ))
+                .build();
+
+        StepVerifier
+                .create(fileDispatchService.dispatchFiles(
+                        List.of(dokumentbeskrivelseDto))
+                )
+                .expectNext(FilesDispatchResult.accepted(
+                        Map.of(
+                                fileMocks.get(0).fileId, fileMocks.get(0).archiveLink,
+                                fileMocks.get(1).fileId, fileMocks.get(1).archiveLink
+                        )
+                )).verifyComplete();
+
+        verifyGetFileAndPostFile(fileMocks);
+
+        verifyNoMoreInteractions(fileClient);
+        verifyNoMoreInteractions(fintArchiveClient);
+    }
+
+    @Test
+    public void givenMultipleDokumentBeskrivelseWithSingleDokumentObjektShouldReturnAcceptedResultWithMultipleDispatch() {
+        List<FileMock> fileMocks = mockFiles(2);
+
+        DokumentbeskrivelseDto dokumentbeskrivelseDto1 = DokumentbeskrivelseDto
+                .builder()
+                .dokumentobjekt(List.of(
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(0).fileId)
+                                .build()
+                ))
+                .build();
+        DokumentbeskrivelseDto dokumentbeskrivelseDto2 = DokumentbeskrivelseDto
+                .builder()
+                .dokumentobjekt(List.of(
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(1).fileId)
+                                .build()
+                ))
+                .build();
+
+        StepVerifier.create(
+                        fileDispatchService.dispatchFiles(List.of(dokumentbeskrivelseDto1, dokumentbeskrivelseDto2))
+                )
+                .expectNext(FilesDispatchResult.accepted(
+                        Map.of(
+                                fileMocks.get(0).fileId, fileMocks.get(0).archiveLink,
+                                fileMocks.get(1).fileId, fileMocks.get(1).archiveLink
+                        ))
+                )
+                .verifyComplete();
+
+        verifyGetFileAndPostFile(fileMocks);
+
+        verifyNoMoreInteractions(fileClient);
+        verifyNoMoreInteractions(fintArchiveClient);
+    }
+
+
+    @Test
+    public void givenMultipleDokumentBeskrivelseWithMultipleDokumentObjektShouldReturnAcceptedResultWithMultipleDispatch() {
+        List<FileMock> fileMocks = mockFiles(5);
+
+        DokumentbeskrivelseDto dokumentbeskrivelseDto1 = DokumentbeskrivelseDto
+                .builder()
+                .dokumentobjekt(List.of(
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(0).fileId)
+                                .build(),
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(1).fileId)
+                                .build()
+                ))
+                .build();
+        DokumentbeskrivelseDto dokumentbeskrivelseDto2 = DokumentbeskrivelseDto
+                .builder()
+                .dokumentobjekt(List.of(
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(2).fileId)
+                                .build(),
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(3).fileId)
+                                .build(),
+                        DokumentobjektDto
+                                .builder()
+                                .fileId(fileMocks.get(4).fileId)
+                                .build()
+                ))
+                .build();
+
+        StepVerifier.create(
+                        fileDispatchService.dispatchFiles(List.of(
+                                dokumentbeskrivelseDto1,
+                                dokumentbeskrivelseDto2
+                        ))
+                ).expectNext(FilesDispatchResult.accepted(fileMocks.stream()
+                        .collect(toMap(
+                                fileMock -> fileMock.fileId,
+                                fileMock -> fileMock.archiveLink
+                        ))
+
+                ))
+                .verifyComplete();
+
+        verifyGetFileAndPostFile(fileMocks);
+
+        verifyNoMoreInteractions(fileClient);
+        verifyNoMoreInteractions(fintArchiveClient);
+    }
+
+
+    private void verifyGetFileAndPostFile(List<FileMock> fileMocks) {
+        IntStream.range(0, fileMocks.size()).forEach(i -> {
+            verify(fileClient, times(1).description("Verify error for fileMock with index=" + i))
+                    .getFile(fileMocks.get(i).fileId);
+            verify(fintArchiveClient, times(1).description("Verify error for fileMock with index=" + i))
+                    .postFile(fileMocks.get(i).file);
+        });
+    }
+
+    private List<FileMock> mockFiles(int numberOfFiles) {
+        return IntStream.range(0, numberOfFiles)
+                .mapToObj(i -> mockFile())
+                .toList();
+    }
+
+    private FileMock mockFile() {
+        FileMock fileMock = new FileMock(
+                mock(UUID.class),
+                mock(File.class),
+                mock(Link.class)
+        );
+        doReturn(Mono.just(fileMock.file)).when(fileClient).getFile(fileMock.fileId);
+        doReturn(Mono.just(fileMock.archiveLink)).when(fintArchiveClient).postFile(fileMock.file);
+
+        return fileMock;
+    }
+
+    @AllArgsConstructor
+    private static class FileMock {
+        private final UUID fileId;
+        private final File file;
+        private final Link archiveLink;
     }
 
 }
